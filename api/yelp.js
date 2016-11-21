de2f9cea4e0d06bf2857fb2e3366cb5b296d5cb8
avnibarman <avni.barman@gmail.com>
2016-11-20 23:02:34 -0800

    submitted

---
+++ api/yelp.js
@@ -0,0 +1,19 @@
+
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  token: process.env.access_token_key,
+  token_secret: process.env.access_token_secret
+});
+
+module.exports = {
+  search: function(searchOptions) {
+
+  var promise = yelp.search(searchOptions);
+
+
+  return promise;
+  }
+};
