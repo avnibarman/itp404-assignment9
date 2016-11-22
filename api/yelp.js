45494ff87b801c8cd7f5217d842a700d9c79fc42
avnibarman <avni.barman@gmail.com>
2016-11-21 22:51:50 -0800

    submitted

--- api/yelp.js
+++ api/yelp.js
@@ -4,8 +4,8 @@ var Yelp = require('yelp');
 var yelp = new Yelp({
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
-  token: process.env.access_token_key,
-  token_secret: process.env.access_token_secret
+  token: process.env.token,
+  token_secret: process.env.token_secret
 });
 
 module.exports = {
